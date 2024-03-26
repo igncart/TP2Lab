813ebac2c3af5328241c4e8bc557f8a073b2f7a9
igncart <ignaciooarrieta@gmail.com>
2024-03-25 21:52:42 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/tp2lab/TP2Lab.java
@@ -0,0 +1,15 @@
+package com.mycompany.tp2lab;
+
+import java.util.Scanner;
+
+public class TP2Lab {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ingrese su nombre.");
+        
+        String nombre = scanner.nextLine();
+    }
+}
